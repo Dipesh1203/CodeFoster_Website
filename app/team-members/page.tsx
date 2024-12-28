8585e9a5d02ce7cf889eda476d92a1eaabf10ec1
Sumit Khandegar <122174778+sumitkhandegar@users.noreply.github.com>
2024-12-28 21:55:22 +0530

    Add team-members images (#6)
    * feat: create team-member-template
    
    * fix: add images of team-members

--- app/team-members/page.tsx
+++ app/team-members/page.tsx
@@ -1,72 +1,93 @@
 import React from "react";
 import TeamMemberCard from "../components/TeamMemberCard";
-import { headTeam, graphicTeam, cpTeam, webTeam, alumni } from "./teamData";
+import {
+  managementTeam,
+  graphicTeam,
+  cpTeam,
+  webTeam,
+  mlTeam,
+  alumni,
+} from "./teamData";
 
 const TeamMembers = () => {
   return (
-    <div className="container mx-auto py-8">
+    <div className="container mx-auto py-8 bg-[#1c2e4a] text-white">
       {/* Main Heading */}
-      <h1 className="text-4xl font-bold text-center mb-8">OUR TEAM</h1>
+      <h1 className="text-5xl font-extrabold text-center mb-12 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 text-transparent bg-clip-text shadow-lg">
+        OUR TEAM
+      </h1>
 
       {/* Head Section */}
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-center bg-gradient-to-r from-purple-300 via-purple-600 to-purple-300 bg-opacity-75 py-2">
-          Head
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-purple-300 via-purple-600 to-purple-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Management Team
         </h2>
         <div className="flex flex-wrap justify-center">
-          {headTeam.map((member, index) => (
-            <TeamMemberCard key={index} {...member} />
-          ))}
-        </div>
-      </section>
-
-      {/* Graphic Team Section */}
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-center bg-gradient-to-r from-blue-300 via-blue-600 to-blue-300 bg-opacity-75 py-2">
-          Graphic Team
-        </h2>
-        <div className="flex flex-wrap justify-center">
-          {graphicTeam.map((member, index) => (
+          {managementTeam?.map((member, index) => (
             <TeamMemberCard key={index} {...member} />
           ))}
         </div>
       </section>
 
       {/* CP Team Section */}
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-center bg-gradient-to-r from-green-300 via-green-600 to-green-300 bg-opacity-75 py-2">
-          CP Team
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-green-300 via-green-600 to-green-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Competitive Programming Team
         </h2>
         <div className="flex flex-wrap justify-center">
-          {cpTeam.map((member, index) => (
+          {cpTeam?.map((member, index) => (
             <TeamMemberCard key={index} {...member} />
           ))}
         </div>
       </section>
 
       {/* Web Team Section */}
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-center bg-gradient-to-r from-yellow-300 via-yellow-600 to-yellow-300 bg-opacity-75 py-2">
-          Web Team
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-yellow-300 via-yellow-600 to-yellow-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Web Development Team
         </h2>
         <div className="flex flex-wrap justify-center">
-          {webTeam.map((member, index) => (
+          {webTeam?.map((member, index) => (
+            <TeamMemberCard key={index} {...member} />
+          ))}
+        </div>
+      </section>
+
+      {/* ML Team Section */}
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-teal-300 via-teal-600 to-teal-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Machine Learning Team
+        </h2>
+        <div className="flex flex-wrap justify-center">
+          {mlTeam?.map((member, index) => (
+            <TeamMemberCard key={index} {...member} />
+          ))}
+        </div>
+      </section>
+
+      {/* Graphic Team Section */}
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-blue-300 via-blue-600 to-blue-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Graphic & Content Team
+        </h2>
+        <div className="flex flex-wrap justify-center">
+          {graphicTeam?.map((member, index) => (
             <TeamMemberCard key={index} {...member} />
           ))}
         </div>
       </section>
 
       {/* Alumni Section */}
-      <section className="mb-8">
-        <h2 className="text-2xl font-semibold mb-4 text-center bg-gradient-to-r from-red-300 via-red-600 to-red-300 bg-opacity-75 py-2">
-          Alumni
+      {/* <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-center bg-gradient-to-r from-red-300 via-red-600 to-red-300 bg-opacity-90 py-3 rounded-lg shadow-md">
+          Alumnies
         </h2>
         <div className="flex flex-wrap justify-center">
-          {alumni.map((member, index) => (
+          {alumni?.map((member, index) => (
             <TeamMemberCard key={index} {...member} />
           ))}
         </div>
-      </section>
+      </section> */}
     </div>
   );
 };
